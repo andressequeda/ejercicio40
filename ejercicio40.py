4fc47afbc24d32a04ef07cba12682c41ce4e1958
Andres Sequeda Higuita <80718240+andressequeda@users.noreply.github.com>
2021-05-10 10:01:57 -0500

    Add files via upload

---
+++ ejercicio40.py
@@ -0,0 +1,7 @@
+numero1  =  int ( input ( "Ingrese el primer número:" ))
+numero2  =  int ( input ( "Ingrese el segundo número:" ))
+numero3  =  int ( input ( "Ingrese el tercer número:" ))
+if  numero1  ==  numero2  o  numero1  ==  numero3  o  numero2  ==  numero3 :
+    print ( "Al menos dos de los números son iguales." )
+otra cosa :
+    print ( "No hay números iguales." )
